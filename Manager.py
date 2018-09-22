5f27ebb4871a3c1dd2dad29b49575fd611f78b8f
TechdudeGames <techdudegames@users.noreply.github.com>
2018-09-22 08:40:02 -0600

    Gui update. Now handles the server port and dissallows stupidity by entering bad input

--- Manager.py
+++ Manager.py
@@ -6,35 +6,35 @@ import os
 import xml.etree.cElementTree as elementtree
 import platform
 if (os.path.isfile('data.xml')):
-	datafile = elementtree.ElementTree(file="data.xml")
-	dataroot = datafile.getroot()
-	# Getting the password and availible senders
-	allowed_senders = []
-	serverpass = None
-	servercommand = None
-	serverdir = None
-	serverport = None
-	for tag in dataroot:
-		if tag.tag == "serverdirectory":
-			serverdir = tag.text
-		if tag.tag == "serverpass":
-			serverpass = tag.text
-		if tag.tag == "servercommand":
-			servercommand = tag.text
-		if tag.tag == "allowed_email":
-			allowed_senders.append(tag.text)
-		if tag.tag == "port":
-			serverport = tag.text
-	
-	currentdir = os.getcwd()
-	
-	
 	class managergui:
 		def __init__(self, master):
+			#Some carp right here
+			datafile = elementtree.ElementTree(file="data.xml")
+			dataroot = datafile.getroot()
+			# Getting the password and availible senders
+			allowed_senders = []
+			serverpass = None
+			servercommand = None
+			serverdir = None
+			serverport = None
+			current = os.getcwd()
+			for tag in dataroot:
+				if tag.tag == "serverdirectory":
+					serverdir = tag.text
+				if tag.tag == "serverpass":
+					serverpass = tag.text
+				if tag.tag == "servercommand":
+					servercommand = tag.text
+				if tag.tag == "allowed_email":
+					allowed_senders.append(tag.text)
+				if tag.tag == "port":
+					serverport = tag.text
+					
+			#Now for the window stuff
 			self.master = master
 			# 1: Create a builder
 			self.builder = builder = pygubu.Builder()
-			builder.add_from_file(os.path.join(currentdir, "gui", "manager.ui"))
+			builder.add_from_file(os.path.join(current, "gui", "manager.ui"))
 			self.mainwindow = builder.get_object('GUI', master)
 			builder.get_variable("serverpass").set(serverpass)
 			builder.get_variable("fullcmd").set(serverdir+servercommand)
@@ -48,55 +48,74 @@ if (os.path.isfile('data.xml')):
 			builder.connect_callbacks(callbacks)
 		
 		def changepass(self):
-			serverpass_parse = elementtree.parse("data.xml")
-			serverpass_elm = serverpass_parse.findall("serverpass")[0]
-			serverpass_elm.text = self.builder.get_variable("newpass").get()
-			serverpass_parse.write("data.xml")
-			self.builder.get_variable("serverpass").set(self.builder.get_variable("newpass").get())
+			newpass = self.builder.get_variable("newpass").get()
+			if newpass.__len__() > 0:
+				serverpass_parse = elementtree.parse("data.xml")
+				serverpass_elm = serverpass_parse.findall("serverpass")[0]
+				serverpass_elm.text = newpass
+				serverpass_parse.write("data.xml")
+				self.builder.get_variable("serverpass").set(newpass)
+			else:
+				messagebox.showerror(title="Error", message="I am not about to let you have a null password.")
 		
 		def changeport(self):
+			newport = self.builder.get_variable("newport").get()
 			serverport_parse = elementtree.parse("data.xml")
 			if serverport_parse.findall("port").__len__() != 0:
-				if type(self.builder.get_variable("newport").get()) == type(int):
-					serverport_elm = serverport_parse.findall("port")[0]
-					serverport_elm.text = self.builder.get_variable("newport").get()
-					serverport_parse.write("data.xml")
-					self.builder.get_variable("currentport").set(self.builder.get_variable("newport").get())
-				elif type(self.builder.get_variable("newport").get()) == type(str):
-					snarkyremark = "Last time I checked " + self.builder.get_variable("newport").get() + "is not a valid port"
-					messagebox.showerror(title="Error", message=snarkyremark)
+				try:
+					if newport.__len__()>5:
+						confirmlargeport = messagebox.askyesno(title="Confirm Large Port", message="Are you sure you want a large port #?")
+						if confirmlargeport:
+							serverport_elm = serverport_parse.findall("port")[0]
+							serverport_elm.text = str(int(newport))
+							serverport_parse.write("data.xml")
+							self.builder.get_variable("currentport").set(newport)
+					elif newport.__len__()<6:
+						serverport_elm = serverport_parse.findall("port")[0]
+						serverport_elm.text = str(int(newport))
+						serverport_parse.write("data.xml")
+						self.builder.get_variable("currentport").set(newport)
+				except ValueError:
+					if newport == "":
+						snarkyremark = "Sorry sir/mam, no blank ports."
+						messagebox.showerror(title="Error", message=snarkyremark)
+					else:
+						snarkyremark = "Last time I checked " + self.builder.get_variable("newport").get() + " is not a valid port."
+						messagebox.showerror(title="Error", message=snarkyremark)
 			else:
 				messagebox.showerror(title="Error",message="The data.xml file does not contain the port tag")
 		
 		def changecmd(self):
 			fullcmd = self.builder.get_variable("newcmd").get()
-			fullcmdlist = list(fullcmd)
-			fullcmdlist.reverse()
-			if platform.win32_ver() == ('', '', '', ''):
-				lastslash = fullcmdlist.index("/")
+			if fullcmd.__len__() > 0:
+				fullcmdlist = list(fullcmd)
+				fullcmdlist.reverse()
+				if platform.win32_ver() == ('', '', '', ''):
+					lastslash = fullcmdlist.index("/")
+				else:
+					lastslash = fullcmdlist.index("\\")
+				onlycmd = fullcmdlist[:lastslash]
+				onlycmd.reverse()
+				if platform.win32_ver() == ('', '', '', ''):
+					onlycmd.insert(0, "./")
+				else:
+					lastslash = fullcmdlist.index(".\\")
+				cmd = ''.join(onlycmd)
+				onlypath = fullcmdlist[lastslash:]
+				onlypath.reverse()
+				path = ''.join(onlypath)
+				serverdir_parse = elementtree.parse("data.xml")
+				serverdir_elm = serverdir_parse.findall("serverdirectory")[0]
+				serverdir_elm.text = path
+				serverdir_parse.write("data.xml")
+				#
+				servercmd_parse = elementtree.parse("data.xml")
+				servercmd_elm = servercmd_parse.findall("servercommand")[0]
+				servercmd_elm.text = cmd
+				servercmd_parse.write("data.xml")
+				self.builder.get_variable("fullcmd").set(path + cmd)
 			else:
-				lastslash = fullcmdlist.index("\\")
-			onlycmd = fullcmdlist[:lastslash]
-			onlycmd.reverse()
-			if platform.win32_ver() == ('', '', '', ''):
-				onlycmd.insert(0, "./")
-			else:
-				lastslash = fullcmdlist.index(".\\")
-			cmd = ''.join(onlycmd)
-			onlypath = fullcmdlist[lastslash:]
-			onlypath.reverse()
-			path = ''.join(onlypath)
-			serverdir_parse = elementtree.parse("data.xml")
-			serverdir_elm = serverdir_parse.findall("serverdirectory")[0]
-			serverdir_elm.text = path
-			serverdir_parse.write("data.xml")
-			#
-			servercmd_parse = elementtree.parse("data.xml")
-			servercmd_elm = servercmd_parse.findall("servercommand")[0]
-			servercmd_elm.text = cmd
-			servercmd_parse.write("data.xml")
-			self.builder.get_variable("fullcmd").set(path+cmd)
-	
+				messagebox.showerror(title="Invalid command", message="Dude, you can't have an empty cmd bro.")
 	
 	if __name__ == '__main__':
 		root = tk.Tk()
